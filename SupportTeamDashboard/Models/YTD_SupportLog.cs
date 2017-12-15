26db0c83c469a6c56dab957f06207ee01e466a9a
Mauricio Guzman <programmingrover@gmail.com>
2017-12-14 17:38:47 -0800

    Updated YTD_SupportLog.cs file to encompass more db categories.

--- SupportTeamDashboard/Models/YTD_SupportLog.cs
+++ SupportTeamDashboard/Models/YTD_SupportLog.cs
@@ -8,21 +8,28 @@ namespace SupportTeamDashboard.Models
     public class YTD_SupportLog
     {
         public int ID { get; set; }
-        public string Quarter { get; set; }
+        public string Project { get; set; }
         public DateTime SupportTicketDate { get; set; }
-        public string AfterHours { get; set; }
         public string SupportTicketType { get; set; }
+        public string AfterHours { get; set; }
+        public string Quarter { get; set; }
+        public string TicketStatus { get; set; }
         public string PartnerName { get; set; }
         public string EnvironmentAffected { get; set; }
-        public string SupportTeamMember { get; set; }
+        public string Urgency { get; set; }
+        public string SupportTeamMember { get; set; } 
+        public string WhoIsCaller { get; set; }
+        public string CallerNumber { get; set; }
+        public string CallerEmail { get; set; }
+        public string TicketDescription { get; set; }
+        public string TicketResolution { get; set; }
         public string SupportTicketCategory { get; set; }
         public string SupportTicketSubCategory { get; set; }
-        public string SupportTicketIssueDetails { get; set; }
-        public string SupportTicketResolutionDetails { get; set; }
         public int QATimeSpent { get; set; }
         public int PTTimeSpent { get; set; }
         public int DevTimeSpent { get; set; }
         public int TotalTimeSpent { get; set; }
+
 
     }
 }
