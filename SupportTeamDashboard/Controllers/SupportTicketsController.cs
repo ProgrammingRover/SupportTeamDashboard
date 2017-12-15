d0d94e138b8a723b66c5131b357941c9cea8af8f
Mauricio Guzman <programmingrover@gmail.com>
2017-12-14 18:45:13 -0800

    Adding data model class and scaffolding controller.

---
+++ SupportTeamDashboard/Controllers/SupportTicketsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SupportTeamDashboard.Models;
+
+namespace SupportTeamDashboard.Controllers
+{
+    public class SupportTicketsController : Controller
+    {
+        private readonly SupportTeamDashboardContext _context;
+
+        public SupportTicketsController(SupportTeamDashboardContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SupportTickets
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SupportTicket.ToListAsync());
+        }
+
+        // GET: SupportTickets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supportTicket = await _context.SupportTicket
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (supportTicket == null)
+            {
+                return NotFound();
+            }
+
+            return View(supportTicket);
+        }
+
+        // GET: SupportTickets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SupportTickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,TicketDate,ProjectType,TicketStatus,TicketUrgency,SupportTeamMember,WhoIsCaller,CallerNumber,CallerEmail,TicketSubject,TicketDescription,TicketResolution,FollowUpRequired,VoicemailCallback,TicketCategory,TicketSubCategory,TotalTimeSpent")] SupportTicket supportTicket)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(supportTicket);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supportTicket);
+        }
+
+        // GET: SupportTickets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supportTicket = await _context.SupportTicket.SingleOrDefaultAsync(m => m.ID == id);
+            if (supportTicket == null)
+            {
+                return NotFound();
+            }
+            return View(supportTicket);
+        }
+
+        // POST: SupportTickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,TicketDate,ProjectType,TicketStatus,TicketUrgency,SupportTeamMember,WhoIsCaller,CallerNumber,CallerEmail,TicketSubject,TicketDescription,TicketResolution,FollowUpRequired,VoicemailCallback,TicketCategory,TicketSubCategory,TotalTimeSpent")] SupportTicket supportTicket)
+        {
+            if (id != supportTicket.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(supportTicket);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupportTicketExists(supportTicket.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supportTicket);
+        }
+
+        // GET: SupportTickets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supportTicket = await _context.SupportTicket
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (supportTicket == null)
+            {
+                return NotFound();
+            }
+
+            return View(supportTicket);
+        }
+
+        // POST: SupportTickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var supportTicket = await _context.SupportTicket.SingleOrDefaultAsync(m => m.ID == id);
+            _context.SupportTicket.Remove(supportTicket);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SupportTicketExists(int id)
+        {
+            return _context.SupportTicket.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ SupportTeamDashboard/Models/SupportTicket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupportTeamDashboard.Models
+{
+    public class SupportTicket
+    {
+        public int ID { get; set; }
+        public DateTime TicketDate { get; set; }
+        public string ProjectType { get; set; }
+        public string TicketStatus { get; set; }
+        public string TicketUrgency { get; set; }
+        public string SupportTeamMember { get; set; }
+        public string WhoIsCaller { get; set; }
+        public int CallerNumber { get; set; }
+        public string CallerEmail { get; set; }
+        public string TicketSubject { get; set; }
+        public string TicketDescription { get; set; }
+        public string TicketResolution { get; set; }
+        public bool FollowUpRequired { get; set; }
+        public bool VoicemailCallback { get; set; }
+        public string TicketCategory { get; set; }
+        public string TicketSubCategory { get; set; }
+        public int TotalTimeSpent { get; set; }
+
+    }
+}
